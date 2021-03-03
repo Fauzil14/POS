00562dc67f1eea19f6192ddd862bf33b530dada0
Fauzil14 <fauzilazim39@gmail.com>
2021-03-03 10:19:27 +0700

    add shift controller

--- app/Http/Resources/ShiftResource.php
+++ app/Http/Resources/ShiftResource.php
@@ -16,9 +16,10 @@ class ShiftResource extends JsonResource
     {
 
         return [
+            'kasir_id'                 => $this->kasir_id,
             'kode_kasir'               => $this->kasir->kode_user,
             'start_time'               => $this->start_time,
-            'end_time'                 => $this->end_time->toDateTimeString(),
+            'end_time'                 => $this->when($this->end_time != null, $this->end_time->toDateTimeString()),
             'transaction_on_shift'     => $this->transaction_on_shift,
             'total_penjualan_on_shift' => $this->total_penjualan_on_shift,
             'duration'                 => $this->when($this->end_time != null, $this->end_time->timespan($this->start_time))
