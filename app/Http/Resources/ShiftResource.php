52f66663927787f74c71feef50bdcdadede2481d
Fauzil14 <fauzilazim39@gmail.com>
2021-03-04 14:38:34 +0700

    add default shift

--- app/Http/Resources/ShiftResource.php
+++ app/Http/Resources/ShiftResource.php
@@ -20,7 +20,7 @@ class ShiftResource extends JsonResource
             'kasir_id'                 => $this->kasir_id,
             'kode_kasir'               => $this->kasir->kode_user,
             'start_time'               => $this->start_time,
-            'end_time'                 => $this->when(Route::currentRouteName() == "api-shift-start", $this->end_time->toDateTimeString()),
+            'end_time'                 => is_null($this->end_time) ? $this->end_time : $this->end_time->toDateTimeString(),
             'transaction_on_shift'     => $this->transaction_on_shift,
             'total_penjualan_on_shift' => $this->total_penjualan_on_shift,
             'duration'                 => $this->when(is_null($this->end_time) == false, $this->end_time->timespan($this->start_time))
