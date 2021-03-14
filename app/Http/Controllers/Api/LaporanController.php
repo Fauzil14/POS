56885803766635eea1338a6b0ef4710e6f5cc972
Fauzil14 <fauzilazim39@gmail.com>
2021-03-14 11:23:28 +0700

    laporan absensi kasir

--- app/Http/Controllers/Api/LaporanController.php
+++ app/Http/Controllers/Api/LaporanController.php
@@ -344,11 +344,11 @@ class LaporanController extends Controller
                 });
                 $shift = $shift->map(function($item, $key) {
                     $kasir = $item->groupBy('kasir_id');
-                    $shift_by_day[] = $kasir->map(function($item,$key) {
+                    $shift_by_day = $kasir->map(function($item,$key) {
                         $new = $this->processAbsensiKasirByDay($item->where('kasir_id', $key)->first());
                         return $new;
                     });
-                    $new = array_merge([ 'minggu_ke' => $key ], $shift_by_day->values()->all());
+                    $new = array_merge([ 'minggu_ke' => $key ], [$shift_by_day->values()->all()]);
                     return $new;
                 })->values()->all();
                 $waktu = "bulan " . Carbon::parse($waktu)->translatedFormat('F Y');
